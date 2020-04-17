cbdef87c72750545bbd44d3334675a5bd9eaa509
majo <maria.riofrio.mr@gmail.com>
2020-04-16 20:10:18 -0400

    fixed issues

--- jscript.js
+++ jscript.js
@@ -1,11 +1,18 @@
 const APIKEY = '2341174992ef15f8815d760242c3e462';
 
 var searchhistory =JSON.parse(localStorage.getItem("weatherApp"))
-var city=searchhistory[0] || "atlanta"
-searchhistory.forEach(function(oldcity){
-    let $city= $("<p>").text(oldcity)
-    $('#list').append($city)
-})
+console.log(searchhistory)
+var city;
+if(searchhistory[0]){
+    console.log("there was a history")
+    city=searchhistory[0]
+    searchhistory.forEach(function(oldcity){
+        let $city= $("<p>").text(oldcity.city)
+        $('#list').append($city)
+    })
+ }else{
+     city="atlanta"
+    }
 
 $("#list p").on("click", function(){
     var cityValue = $(this).text();
@@ -15,8 +22,8 @@ $("#list p").on("click", function(){
     // Run an ajax with the value the text to get the new weather
 
 })
-
-getWeather("atlanta")
+console.log(city)
+getWeather(city.city)
 function getWeather(city){
 $.get(
 `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=imperial`,
@@ -75,21 +82,20 @@ $("#search").on("click", function(event){
     event.preventDefault()
     var input = $('#input').val()
     let oldStorage= JSON.parse(localStorage.getItem("weatherApp"))
-    if (!oldStorage.selected(input)) {
-        oldStorage.unshift(input)
+    //if (!oldStorage.selected(input)) {
+        console.log("outside",oldStorage)
+    if (oldStorage) {
+        console.log("inside",oldStorage)
+        oldStorage.unshift({city:input})
+        if (oldStorage.length > 10 ){
+            oldStorage.pop()
+        }
         localStorage.setItem("weatherApp", JSON.stringify(oldStorage))
+    }else{
+        console.log("Good thing we have the local storage got deleted case/ the default case")
+        localStorage.setItem("weatherApp", JSON.stringify([{city:input}]))       
     }
     city=input
     getWeather(input)
     console.log(input)
 })
-// $("#search").on("click", function(event){
-//     event.preventDefault()
-//     var input = $('#input').val()
-//     let oldStorage= JSON.parse(localStorage.getItem("weatherApp"))
-//     oldStorage.unshift(input)
-//     localStorage.setItem("weatherApp", JSON.stringify(oldStorage))
-//     city=input
-//     getWeather()
-//     console.log(input)
-// })
